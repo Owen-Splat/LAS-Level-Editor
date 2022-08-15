01b71775e4d483aacc3ec5c3013d79da4626b6a2
OSmart32 <Owen32Smart@gmail.com>
2022-08-15 19:52:28 -0400

    allow keyboard interrupts

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
-from PySide6 import QtWidgets
+from PySide6 import QtCore, QtWidgets
 import Ui.main_window as window
 import sys
 
+def interruptHandler(sig, frame):
+    sys.exit(0)
+
+# Allow keyboard interrupts
+import signal
+signal.signal(signal.SIGINT, interruptHandler)
+
 app = QtWidgets.QApplication([])
 app.setStyle('Fusion')
+
+timer = QtCore.QTimer()
+timer.start(100)
+timer.timeout.connect(lambda: None)
 
 m = window.MainWindow()
 sys.exit(app.exec())
